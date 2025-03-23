f7e35c3d45af8f8db14de3a58c77339d7dd002b2
charbarnes <charbarnz@gmail.com>
2025-03-23 18:34:20 -0500

    Changing default page

---
+++ src/app/head.tsx
@@ -0,0 +1,9 @@
+// src/app/head.tsx
+export default function Head() {
+  return (
+    <>
+      <title>QueueView</title>
+      <meta name="description" content="Live TSA-line wait tracker." />
+    </>
+  );
+}
